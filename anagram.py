24c10778f9f4be9fdff3b1b7c773b0330e2fa2a9
Alessandro Pisa <alessandro.pisa@redturtle.it>
2016-01-09 19:42:39 +0100

    Do the minimum

--- anagram.py
+++ anagram.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 import sys
 import tarfile
-ita_dict = {}
 
-# this is CPU waste... but the mapping can be useful for a future version
-with tarfile.open(u'italia-1a.gz', 'r:gz') as tar:
-    for member in tar.getmembers():
-        for line in tar.extractfile(member).read().decode('utf8').split():
-            line = str(line.strip().lower())
-            key = ''.join(sorted(set(line)))
-            ita_dict.setdefault(key, []).append(line)
-
-word = "".join(sys.argv[1:]).lower()
-word_key = ''.join(sorted(set(word)))
+word = ''.join(sys.argv[1:]).lower()
+word_key = set(word)
 word_sorted = sorted(word)
 
-for possible_match in ita_dict.get(word_key, []):
-    if sorted(possible_match) == word_sorted:
-        print(possible_match)
+with tarfile.open('italia-1a.gz') as tar:
+    for member in tar.getmembers():
+        lines = tar.extractfile(member).read().decode('utf8').lower().split()
+        for line in lines:
+            if sorted(line) == word_sorted:
+                print(line)
